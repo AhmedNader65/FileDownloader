dee5b91e367516927b2027ce0fc2f3972f2835a5
Ahmed <ahmed.nader1994@gmail.com>
2022-04-21 20:09:14 +0200

    add hilt annotations

--- app/src/main/java/com/najwa/task/ui/MainActivity.kt
+++ app/src/main/java/com/najwa/task/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.najwa.task.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.najwa.task.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
